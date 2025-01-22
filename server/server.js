8b2dbc60c09b2ee35459e188b6bbe5230e74931e
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-22 13:46:34 +0500

    Create Database connection

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
- 
+
 app.use(
 	cors({
 		origin: process.env.CLIENT_URL,
@@ -16,13 +16,13 @@ app.use(
 );
 
 app.use(express.json());
-/*
+
 //database connection
 mongoose
 	.connect(MONGO_URI)
 	.then(() => console.log("mongodb is connected"))
 	.catch((e) => console.log(e));
-
+/*
 app.use((err, req, res, next) => {
 	console.log(err.stack);
 	res.status(500).json({
