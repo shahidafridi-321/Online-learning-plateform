92604cb7e4dfb2961125b3403d48193012b4d4ca
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-22 13:45:23 +0500

    Inicialize express app, PORT and MONGO_URL

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-/* 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
-
+/* 
 app.use(
 	cors({
 		origin: process.env.CLIENT_URL,
