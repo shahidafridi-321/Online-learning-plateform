d5ef65682a72b1864c5adf7ba95ac6e7a119a1c7
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-24 08:29:14 +0500

    Wrap the App component inside the AuthProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/Router";
 import App from "./App";
+import AuthProvider from "./context/auth-context";
 
 createRoot(document.getElementById("root")).render(
 	<RouterProvider router={routes}>
-		<App />
+		<AuthProvider>
+			<App />
+		</AuthProvider>
 	</RouterProvider>
 );
