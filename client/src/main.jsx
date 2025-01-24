5393c7a07451a4fd1ead95460d88d4c435b513d5
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-24 18:40:59 +0500

    Remove The Authprovider component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/Router";
-import AuthProvider from "./context/auth-context";
 
 createRoot(document.getElementById("root")).render(
-	<AuthProvider>
-		<RouterProvider router={routes} />
-	</AuthProvider>
+	<RouterProvider router={routes} />
 );
