e9f1129bac6395bee89b5f1f576acf8b633a3632
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-27 09:24:23 +0500

    Create auth route

---
+++ server/routes/auth-routes/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const { registerUser } = require("../../controllers/auth-controller/index");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
