f89aa7f3ef52bf6355dd354e3e512c8eec74b159
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-27 14:47:38 +0500

    Add two methods checkAuthUser and handleLoginUser

--- client/src/context/auth-context/index.jsx
+++ client/src/context/auth-context/index.jsx
@@ -1,18 +1,71 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { initialSignInFormData, initialSignUpFormData } from "@/config";
-import { registerService } from "@/services";
+import { checkAuthService, loginService, registerService } from "@/services";
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
 	const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
 	const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
+	const [auth, setAuth] = useState({
+		authenticate: false,
+		user: null,
+	});
 
 	const handleRegisterUser = async (e) => {
 		e.preventDefault();
 		const data = await registerService(signUpFormData);
-		console.log(data);
+		return data;
 	};
+
+	const handleLoginUser = async (e) => {
+		e.preventDefault();
+		const data = await loginService(signInFormData);
+
+		if (data.success) {
+			sessionStorage.setItem(
+				"accessToken",
+				JSON.stringify(data.data.accessToken)
+			);
+			setAuth({
+				authenticate: true,
+				user: data.data.user,
+			});
+		} else {
+			setAuth({
+				authenticate: false,
+				user: null,
+			});
+		}
+	};
+
+	const checkAuthUser = async () => {
+		try {
+			const data = await checkAuthService();
+			if (data.success) {
+				setAuth({
+					authenticate: true,
+					user: data.data.user,
+				});
+			} else {
+				setAuth({
+					authenticate: false,
+					user: null,
+				});
+			}
+		} catch (error) {
+			console.error("Error checking authentication:", error);
+			setAuth({
+				authenticate: false,
+				user: null,
+			});
+		}
+	};
+
+	useEffect(() => {
+		checkAuthUser();
+	}, []);
+	console.log(auth);
 
 	return (
 		<AuthContext.Provider
@@ -22,6 +75,7 @@ export const AuthProvider = ({ children }) => {
 				signUpFormData,
 				setSignUpFormData,
 				handleRegisterUser,
+				handleLoginUser,
 			}}
 		>
 			{children}
