649681eee7861ed6eca45d53c15f8a05d89e6e2b
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-27 14:52:42 +0500

    Add loging end point and checkauth point

--- server/routes/auth-routes/index.js
+++ server/routes/auth-routes/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const { registerUser } = require("../../controllers/auth-controller/index");
-
+const {
+	registerUser,
+	loginUser,
+} = require("../../controllers/auth-controller/index");
+const authenticateMiddleware = require("../../middleware/auth-middleware");
 const router = express.Router();
 
 router.post("/register", registerUser);
-
+router.post("/login", loginUser);
+router.get("/check-auth", authenticateMiddleware, (req, res) => {
+	const user = req.user;
+	return res.status(201).json({
+		success: true,
+		message: "User is Authenticated",
+		data: { user },
+	});
+});
 module.exports = router;
