67bc7a942f8fb68f38470c375fb9ea4ee7514820
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-27 14:49:56 +0500

    Add login service and checkauth service

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,9 +1,19 @@
-import { axiosInstance } from "@/api/axiosInstance";
+import axiosInstance from "@/api/axiosInstance";
 
 export const registerService = async (formData) => {
 	const { data } = await axiosInstance.post("/auth/register", {
 		...formData,
 		role: "user",
 	});
+	return data;
+};
+
+export const loginService = async (formData) => {
+	const { data } = await axiosInstance.post("/auth/login", formData);
+	return data;
+};
+
+export const checkAuthService = async () => {
+	const { data } = await axiosInstance.get("/auth/check-auth");
 	return data;
 };
