2aa8c9bef2f4da15f18502a09a3401a812e1a727
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-27 10:56:33 +0500

    Create handleRegisterUser  and pass to AuthContext provider

--- client/src/context/auth-context/index.jsx
+++ client/src/context/auth-context/index.jsx
@@ -1,11 +1,18 @@
 import { createContext, useState } from "react";
 import { initialSignInFormData, initialSignUpFormData } from "@/config";
+import { registerService } from "@/services";
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
 	const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
 	const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
+
+	const handleRegisterUser = async (e) => {
+		e.preventDefault();
+		const data = await registerService(signUpFormData);
+		console.log(data);
+	};
 
 	return (
 		<AuthContext.Provider
@@ -14,6 +21,7 @@ export const AuthProvider = ({ children }) => {
 				setSignInFormData,
 				signUpFormData,
 				setSignUpFormData,
+				handleRegisterUser,
 			}}
 		>
 			{children}
