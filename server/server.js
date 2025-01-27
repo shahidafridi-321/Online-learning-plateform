8bb92d2d953d19f7c8a4f72fab6b040a8b09804f
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-27 09:29:51 +0500

    create route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth-routes/index");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -22,6 +23,10 @@ mongoose
 	.connect(MONGO_URI)
 	.then(() => console.log("mongodb is connected"))
 	.catch((e) => console.log(e));
+
+// routes configuration
+app.use("/auth", authRoutes);
+
 
 app.use((err, req, res, next) => {
 	console.log(err.stack);
