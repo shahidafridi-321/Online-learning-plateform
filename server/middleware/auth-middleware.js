048aeea61a0ce483d79c2d6ea8a28d680cc078d4
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-27 14:51:16 +0500

    Create auth middleware

---
+++ server/middleware/auth-middleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (token, secretKey) => {
+	return jwt.verify(token, secretKey);
+};
+
+const authenticate = (req, res, next) => {
+	const authHeaders = req.headers.authorization;
+	if (!authHeaders) {
+		return res.status(401).json({
+			success: false,
+			message: "User is not authenticated",
+		});
+	}
+
+	const token = authHeaders.split(" ")[1];
+	const payload = verifyToken(token, "JWT_SECRET");
+
+	req.user = payload;
+	next();
+};
+
+module.exports = authenticate;
