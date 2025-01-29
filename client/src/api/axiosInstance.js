706638f2851cc79e2a52ffce906b198350f953ca
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-29 18:56:48 +0500

    Fix the issue JSON.parse Unexpected End of Data

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -3,15 +3,25 @@ import axios from "axios";
 const axiosInstance = axios.create({
 	baseURL: "http://localhost:5002",
 });
-
 axiosInstance.interceptors.request.use(
 	(config) => {
-		const accessToken = JSON.parse(sessionStorage.getItem("accessToken") || "");
-		if (accessToken) {
-			config.headers.Authorization = `Bearer ${accessToken}`;
+		const tokenString = sessionStorage.getItem("accessToken");
+
+		if (tokenString) {
+			try {
+				const accessToken = JSON.parse(tokenString); 
+				if (accessToken) {
+					config.headers.Authorization = `Bearer ${accessToken}`;
+				}
+			} catch (error) {
+				console.error("Error parsing accessToken:", error);
+				sessionStorage.removeItem("accessToken");
+			}
 		}
+
 		return config;
 	},
 	(err) => Promise.reject(err)
 );
+
 export default axiosInstance;
