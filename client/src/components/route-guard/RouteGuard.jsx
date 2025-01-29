d8fca375ead183110e9e03eca542e6d59d3d25e3
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-29 20:09:10 +0500

    Render Skeleton

--- client/src/components/route-guard/RouteGuard.jsx
+++ client/src/components/route-guard/RouteGuard.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Fragment } from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import { Skeleton } from "../ui/skeleton";
 
 export const RouteGuard = ({ authenticated, user, element, loading }) => {
 	const location = useLocation();
 
 	if (loading) {
-		return <div>loading...</div>;
+		return <Skeleton />;
 	}
 	if (!authenticated && !location.pathname.includes("/auth")) {
 		return <Navigate to="/auth" />;
