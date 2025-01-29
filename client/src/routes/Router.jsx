336e0f6e576cb363f9196a2fca3fe4c0f6729620
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-29 19:03:34 +0500

    Create ProtectRoute component that renders element for different routes

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,10 +1,46 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "@/App";
 import { AuthPage } from "@/pages/auth";
+import { RouteGuard } from "@/components/route-guard/RouteGuard";
+import { useContext } from "react";
+import { AuthContext } from "@/context/auth-context";
+import { InstructorDashboardPage } from "@/pages/instructor/InstructorDashboardPage";
+import { StudentViewCommonLayout } from "@/components/student-view/StudentViewCommonLayout";
+import { StudentHomePage } from "@/pages/student/StudentHomePage";
+
+const ProtectedRoute = ({ element }) => {
+	const { auth, loading } = useContext(AuthContext);
+	return (
+		<RouteGuard
+			authenticated={auth?.authenticate}
+			user={auth?.user}
+			element={element}
+			loading={loading}
+		/>
+	);
+};
 
 export const routes = createBrowserRouter([
 	{
 		path: "/auth",
-		element: <AuthPage />,
+		element: <ProtectedRoute element={<AuthPage />} />,
+	},
+	{
+		path: "/instructor",
+		element: <ProtectedRoute element={<InstructorDashboardPage />} />,
+	},
+	{
+		path: "/",
+		element: <ProtectedRoute element={<StudentViewCommonLayout />} />,
+		children: [
+			{
+				path: "/",
+				element: <StudentHomePage />,
+			},
+			{
+				path: "/home",
+				element: <StudentHomePage />,
+			},
+		],
 	},
 ]);
