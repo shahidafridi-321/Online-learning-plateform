3eb6fd065064f871efed0ffa16a70b228d12749e
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-29 18:58:52 +0500

    Create RouterGuard component that protects routes based on authentication and user role

---
+++ client/src/components/route-guard/RouteGuard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Fragment } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+export const RouteGuard = ({ authenticated, user, element, loading }) => {
+	const location = useLocation();
+
+	if (loading) {
+		return <div>loading...</div>;
+	}
+	if (!authenticated && !location.pathname.includes("/auth")) {
+		return <Navigate to="/auth" />;
+	}
+	if (
+		authenticated &&
+		user?.role !== "instructor" &&
+		(location.pathname.includes("instructor") ||
+			location.pathname.includes("/auth"))
+	) {
+		return <Navigate to="/home" />;
+	}
+	if (
+		authenticated &&
+		user.role === "instructor" &&
+		!location.pathname.includes("instructor")
+	) {
+		return <Navigate to="/instructor" />;
+	}
+
+	return <Fragment>{element}</Fragment>;
+};
