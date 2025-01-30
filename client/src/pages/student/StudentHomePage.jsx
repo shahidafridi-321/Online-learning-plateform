3e240b88e8336694050c6c18367ba3769ea56d71
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-30 09:03:12 +0500

    Add Logout functionality in studentview

--- client/src/pages/student/StudentHomePage.jsx
+++ client/src/pages/student/StudentHomePage.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import { AuthContext } from "@/context/auth-context";
+import React, { useContext } from "react";
+import { Button } from "@/components/ui/button";
 
 export const StudentHomePage = () => {
-	return <div>StudentHomePage</div>;
+	const { resetCredentials } = useContext(AuthContext);
+	const handleLogout = () => {
+		resetCredentials();
+		sessionStorage.clear();
+	};
+	return (
+		<div>
+			StudentHomePage
+			<Button onClick={handleLogout}>Logout</Button>
+		</div>
+	);
 };
