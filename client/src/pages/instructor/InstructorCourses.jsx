e5d9a47eafc93f2d2fd5a6dd6e58714254257391
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-30 09:16:27 +0500

    Add a Card that has card header and card content component

--- client/src/pages/instructor/InstructorCourses.jsx
+++ client/src/pages/instructor/InstructorCourses.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 export const InstructorCourses = () => {
-  return (
-    <div>InstructorCourses</div>
-  )
-}
+	return (
+		<Card>
+			<CardHeader className="flex justify-between flex-row items-center">
+				<CardTitle className="text-3xl font-extrabold">All Courses</CardTitle>
+				<Button className="p-5">Create New Course</Button>
+			</CardHeader>
+      <CardContent>
+        <div  className="overflow-x-auto">
+          
+
+        </div>
+      </CardContent>
+		</Card>
+	);
+};
