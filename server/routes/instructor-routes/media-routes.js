8942a219c27a2e53dcad609e09a902bbf6082952
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-31 11:22:35 +0500

    Create upload and delete routes for media and export the router

--- server/routes/instructor-routes/media-routes.js
+++ server/routes/instructor-routes/media-routes.js
@@ -9,4 +9,41 @@ const router = express.Router();
 
 const upload = multer({ dest: "uploads/" });
 
+router.post("/upload", upload.single("file"), async (req, res) => {
+	try {
+		const result = await uploadMediaToCloudinary(req.file.path);
+		res.status(200).json({
+			success: true,
+			data: result,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "Error uploading file",
+		});
+	}
+});
 
+router.delete("/delete/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+		if (!id) {
+			res.status(400).json({
+				success: false,
+				message: "Asset id is required",
+			});
+		}
+		await deleteMediaFromCloudinary(id);
+		res.status(200).json({
+			success: true,
+			message: "Asset deleted successfully from cloudinary",
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "Error deleting file",
+		});
+	}
+});
+
+module.exports = router;
