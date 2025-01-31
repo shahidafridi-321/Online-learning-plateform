16b483a23caa48acb2736b73f2e053549b77c72d
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-31 10:39:35 +0500

    Add uploadMediaToCloudinary

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -7,3 +7,16 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+
+const uploadMediaToCloudinary = async (filePath) => {
+	try {
+		const result = await cloudinary.uploader.upload(filePath, {
+			resource_type: "auto",
+		});
+		return result;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Error Uploading Media to cloudinary");
+	}
+};
+
