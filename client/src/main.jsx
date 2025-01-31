2aea9b113c043947e1423f8d4675a48badbc9077
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-31 08:49:49 +0500

    Wrap App inside instructor context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AuthProvider } from "./context/auth-context";
 import App from "./App";
+import { InstructorContextProvider } from "./context/instructor-context/InstructorContext";
 
 createRoot(document.getElementById("root")).render(
 	<AuthProvider>
-		<App />
+		<InstructorContextProvider>
+			<App />
+		</InstructorContextProvider>
 	</AuthProvider>
 );
