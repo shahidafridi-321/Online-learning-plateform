bff28c411b060d38da743ab1ea898d6d29a868f1
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-31 09:11:08 +0500

    Add title and Form

--- client/src/components/instructor-view/CourseLanding.jsx
+++ client/src/components/instructor-view/CourseLanding.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { FormControls } from "../ui/common-form/FormControls";
+import { courseLandingPageFormControls } from "@/config";
+import { InstructorContext } from "@/context/instructor-context/InstructorContext";
 
 export const CourseLanding = () => {
-  return (
-    <div>CourseLanding</div>
-  )
-}
+	const { courseLandingFormData, setCourseLandingFormData } =
+		useContext(InstructorContext);
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Course Landing Page</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<FormControls
+					formControls={courseLandingPageFormControls}
+					formData={courseLandingFormData}
+					setFormData={setCourseLandingFormData}
+				/>
+			</CardContent>
+		</Card>
+	);
+};
