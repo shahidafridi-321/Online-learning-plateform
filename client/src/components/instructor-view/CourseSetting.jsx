ffda9e6cff8e9fa2e64127df572144a3441b1052
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-01-31 09:11:53 +0500

    Add title and file Image uploading functionality

--- client/src/components/instructor-view/CourseSetting.jsx
+++ client/src/components/instructor-view/CourseSetting.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 export const CourseSetting = () => {
-  return (
-    <div>CourseSetting</div>
-  )
-}
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Course Setting Page</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<div className="flex flex-col gap-3">
+					<Label>Upload Course Image</Label>
+					<Input type="file" className="mb-4" accept="image/*" />
+				</div>
+			</CardContent>
+		</Card>
+	);
+};
