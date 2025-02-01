0d966e60164fb7b01d5f439452e9c41450cbe97a
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-01 09:19:19 +0500

    Add handleImageUploadChange that handle image upload

--- client/src/components/instructor-view/CourseSetting.jsx
+++ client/src/components/instructor-view/CourseSetting.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { InstructorContext } from "@/context/instructor-context/InstructorContext";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { mediaUploadService } from "@/services";
 
 export const CourseSetting = () => {
+	const { courseLandingFormData, setCourseLandingFormData } =
+		useContext(InstructorContext);
+
+		
+	const handleImageUploadChange = async (event) => {
+		const seletedImage = event.target.files[0];
+		if (seletedImage) {
+			const imageFormData = new FormData();
+			imageFormData.append("file", seletedImage);
+			try {
+				const response = await mediaUploadService(imageFormData);
+				if (response.success) {
+					setCourseLandingFormData({
+						...courseLandingFormData,
+						image: response.data.url,
+					});
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	};
 	return (
 		<Card>
 			<CardHeader>
@@ -12,7 +36,12 @@ export const CourseSetting = () => {
 			<CardContent>
 				<div className="flex flex-col gap-3">
 					<Label>Upload Course Image</Label>
-					<Input type="file" className="mb-4" accept="image/*" />
+					<Input
+						type="file"
+						className="mb-4"
+						accept="image/*"
+						onChange={(event) => handleImageUploadChange(event)}
+					/>
 				</div>
 			</CardContent>
 		</Card>
