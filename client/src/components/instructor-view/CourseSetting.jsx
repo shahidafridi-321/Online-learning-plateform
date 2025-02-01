3eb210335d0902643a39de3b5a197aac9748a25c
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-01 11:14:24 +0500

    Pass a callback to mediaUploadService

--- client/src/components/instructor-view/CourseSetting.jsx
+++ client/src/components/instructor-view/CourseSetting.jsx
@@ -4,11 +4,16 @@ import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { mediaUploadService } from "@/services";
-import { Image } from "lucide-react";
-
+import { MediaProgressBar } from "../MediaProgressBar";
 export const CourseSetting = () => {
-	const { courseLandingFormData, setCourseLandingFormData } =
-		useContext(InstructorContext);
+	const {
+		courseLandingFormData,
+		setCourseLandingFormData,
+		mediaUploadProgress,
+		setMediaUploadProgress,
+		mediaUploadProgressPercentage,
+		setMediaUploadProgressPercentage,
+	} = useContext(InstructorContext);
 
 	const handleImageUploadChange = async (event) => {
 		const seletedImage = event.target.files[0];
@@ -16,12 +21,17 @@ export const CourseSetting = () => {
 			const imageFormData = new FormData();
 			imageFormData.append("file", seletedImage);
 			try {
-				const response = await mediaUploadService(imageFormData);
+				setMediaUploadProgress(true);
+				const response = await mediaUploadService(
+					imageFormData,
+					setMediaUploadProgressPercentage
+				);
 				if (response.success) {
 					setCourseLandingFormData({
 						...courseLandingFormData,
 						image: response.data.url,
 					});
+					setMediaUploadProgress(false);
 				}
 			} catch (error) {
 				console.log(error);
@@ -35,6 +45,14 @@ export const CourseSetting = () => {
 			<CardHeader>
 				<CardTitle>Course Setting</CardTitle>
 			</CardHeader>
+			<div className="p-4">
+				{mediaUploadProgress ? (
+					<MediaProgressBar
+						isMediaUploading={mediaUploadProgress}
+						progress={mediaUploadProgressPercentage}
+					/>
+				) : null}
+			</div>
 			<CardContent>
 				{courseLandingFormData?.image ? (
 					<img src={courseLandingFormData.image} />
