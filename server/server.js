1d595431875a01f02870bbe88e0e907bd59e5e11
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-03 08:27:41 +0500

    Import instructor course routes and configure it

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const authRoutes = require("./routes/auth-routes/index");
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
+const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -29,6 +30,7 @@ mongoose
 // routes configuration
 app.use("/auth", authRoutes);
 app.use("/media", mediaRoutes);
+app.use("/instructor/course", instructorCourseRoutes);
 
 app.use((req, res, next) => {
 	console.log(`${req.method} ${req.path}`);
