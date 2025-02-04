a3ffdca6ab2e6a9afd17ddbca30737e7b3616c8a
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-04 08:30:51 +0500

    Add Bulk upload route

--- server/routes/instructor-routes/media-routes.js
+++ server/routes/instructor-routes/media-routes.js
@@ -46,4 +46,24 @@ router.delete("/delete/:id", async (req, res) => {
 	}
 });
 
+router.post("/bulk-upload", upload.array[("files", 10)], async (req, res) => {
+	try {
+		const uploadPromises = req.files.map((file) =>
+			uploadMediaToCloudinary(file.path)
+		);
+		const results = await Promise.all(uploadPromises);
+		res.status(200).json({
+			success: true,
+			data: results,
+			message: "All files uploaded successfully to cloudinary",
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: "Error in bulk uploading files",
+		});
+	}
+});
+
 module.exports = router;
