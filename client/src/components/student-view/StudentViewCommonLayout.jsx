98613652c007a54c41f9c3bb8e603ae0071c4d1f
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-05 09:48:26 +0500

    Add styles

--- client/src/components/student-view/StudentViewCommonLayout.jsx
+++ client/src/components/student-view/StudentViewCommonLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Header } from "./Header";
 
 export const StudentViewCommonLayout = () => {
 	return (
-		<div>
-			StudentViewCommonLayout
+		<div className="flex flex-col min-h-screen bg-gradient-to-br from-blue-300 via-purple-300 to-pink-300 text-gray-800">
+			<Header />
 			<Outlet />
 		</div>
 	);
