4dd7e32dc7b0880c0b6289b5837e8da5de7c7676
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-06 09:51:41 +0500

    Fix Calling Wrong function

--- server/routes/student-routes/course-routes.js
+++ server/routes/student-routes/course-routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const {
 	getStudentViewCourseDetails,
+	getAllStudentViewCourses,
 } = require("../../controllers/student-controller/course-controller");
 
 const router = express.Router();
 
-router.get("/get", getStudentViewCourseDetails);
+router.get("/get", getAllStudentViewCourses);
 router.get("/get/details/:id", getStudentViewCourseDetails);
 
 module.exports = router;
