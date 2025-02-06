36e99e660032607af1f331c35fe2e7fb3f6c5c2d
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-06 08:37:43 +0500

    Wrap App component inside the studentcontextprovider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import "./index.css";
 import { AuthProvider } from "./context/auth-context";
 import App from "./App";
 import { InstructorContextProvider } from "./context/instructor-context/InstructorContext";
+import { StudentContextProvider } from "./context/student-context/StudentContext";
 
 createRoot(document.getElementById("root")).render(
 	<AuthProvider>
 		<InstructorContextProvider>
-			<App />
+			<StudentContextProvider>
+				<App />
+			</StudentContextProvider>
 		</InstructorContextProvider>
 	</AuthProvider>
 );
