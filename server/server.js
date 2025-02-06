6b515779499c124f236b438b3b3316c42166ec5f
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-06 09:19:40 +0500

    Configure student course routes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const authRoutes = require("./routes/auth-routes/index");
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
 const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
+const studentCourseRoutes = require("./routes/student-routes/course-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -31,6 +32,7 @@ mongoose
 app.use("/auth", authRoutes);
 app.use("/media", mediaRoutes);
 app.use("/instructor/course", instructorCourseRoutes);
+app.use("/student/course", studentCourseRoutes);
 
 app.use((req, res, next) => {
 	console.log(`${req.method} ${req.path}`);
