7e5f39b32ca65977149e1ae03bfcc3f3389dbaa9
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-06 09:07:03 +0500

    Add getStudentViewCourseDetails to get a single course detials from database

--- server/controllers/student-controller/course-controller.js
+++ server/controllers/student-controller/course-controller.js
@@ -4,7 +4,7 @@ const getAllStudentViewCourses = async (req, res) => {
 	try {
 		const coursesList = await Course.find({});
 		if (coursesList.length === 0) {
-			res.status(404).json({
+			return res.status(404).json({
 				success: false,
 				message: "No Course Found",
 				data: [],
@@ -22,4 +22,34 @@ const getAllStudentViewCourses = async (req, res) => {
 			message: "Something went wrong,fetching courses error",
 		});
 	}
+};
+
+const getStudentViewCourseDetails = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const courseDetails = await Course.findById(id);
+		if (!courseDetails) {
+			return res.status(404).json({
+				success: false,
+				message: "Course Not Found",
+				data: null,
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			data: courseDetails,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: "Something went wrong,fetching course details error",
+		});
+	}
+};
+
+module.exports = {
+	getAllStudentViewCourses,
+	getStudentViewCourseDetails,
 };
