e379e4ee93dee3458f3ef78bc19132bd22315ff8
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-07 09:10:55 +0500

    Add loading, setLoading state and pass it to student context

--- client/src/context/student-context/StudentContext.jsx
+++ client/src/context/student-context/StudentContext.jsx
@@ -4,11 +4,15 @@ export const StudentContext = createContext(null);
 
 export const StudentContextProvider = ({ children }) => {
 	const [studentViewCourseList, setStudentViewCourseList] = useState([]);
+
+	const [loading, setLoading] = useState(false);
 	return (
 		<StudentContext.Provider
 			value={{
 				studentViewCourseList,
 				setStudentViewCourseList,
+				loading,
+				setLoading,
 			}}
 		>
 			{children}
