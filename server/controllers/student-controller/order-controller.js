2f388e537a7d5b2664ae4e38904b9ff6f4fa0c17
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-19 12:18:11 +0500

    Add createOrder function that will create an order

--- server/controllers/student-controller/order-controller.js
+++ server/controllers/student-controller/order-controller.js
@@ -1,2 +1,104 @@
+const paypal = require("../../helpers/paypal");
+const Order = require("../../models/Order");
+const StudentCourses = require("../../models/StudentCourses");
 
+const Course = require("../../models/Course");
 
+const createOrder = async (req, res) => {
+	try {
+		const {
+			userId,
+			userName,
+			userEmail,
+			orderStatus,
+			paymentMethod,
+			paymentStatus,
+			PaymentId,
+			orderDate,
+			payerId,
+			instructorId,
+			instructorName,
+			courseImage,
+			courseTitle,
+			courseId,
+			coursePricing,
+		} = req.body;
+
+		const create_payment_json = {
+			intent: "sale",
+			payer: {
+				payment_method: "paypal",
+			},
+			redirect_urls: {
+				return_url: `${process.env.CLIENT_URL}/payment-return`,
+				cancel_url: `${process.env.CLIENT_URL}/payment-cancel`,
+			},
+			transictions: [
+				{
+					item_list: {
+						items: [
+							{
+								name: courseTitle,
+								sku: courseId,
+								price: coursePricing,
+								currency: "USD",
+								quantity: 1,
+							},
+						],
+					},
+					amount: {
+						currency: "USD",
+						total: coursePricing.toFixed(2),
+					},
+					description: courseTitle,
+				},
+			],
+		};
+
+		paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
+			if (error) {
+				console.log(error);
+				return res.status(500).json({
+					success: false,
+					message: "Error While Creating Payment!",
+				});
+			} else {
+				const newlyCreatedOrder = new Order({
+					userId,
+					userName,
+					userEmail,
+					orderStatus,
+					paymentMethod,
+					paymentStatus,
+					PaymentId,
+					orderDate,
+					payerId,
+					instructorId,
+					instructorName,
+					courseImage,
+					courseTitle,
+					courseId,
+					coursePricing,
+				});
+				await newlyCreatedOrder.save();
+				const approveUrl = paymentInfo.links.find(
+					(link) => link.rel == "approval_url"
+				).href;
+
+				res.status(201).json({
+					success: true,
+					data: {
+						approveUrl,
+						orderId: newlyCreatedOrder._id,
+					},
+				});
+			}
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: "Payment was unsuccessful!",
+		});
+	}
+};
