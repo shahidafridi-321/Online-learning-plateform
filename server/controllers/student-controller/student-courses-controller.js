437fc79a576ba30170fb21bd914699c8e520db23
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-19 20:24:59 +0500

    Fix the wrong asigment for success

--- server/controllers/student-controller/student-courses-controller.js
+++ server/controllers/student-controller/student-courses-controller.js
@@ -8,7 +8,7 @@ const getCoursesByStudentId = async (req, res) => {
 		});
 
 		res.status(200).json({
-			success: false,
+			success: true,
 			data: studentBoughtCourses.courses,
 		});
 	} catch (error) {
