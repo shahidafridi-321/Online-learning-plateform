129782eb6c4d902474a1fd85d30608f6128e81b7
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-19 19:21:36 +0500

    Create student-courses-controller which has a method getCoursesByStudentId

---
+++ server/controllers/student-controller/student-courses-controller.js
@@ -0,0 +1,23 @@
+const StudentCourses = require("../../models/StudentCourses");
+
+const getCoursesByStudentId = async (req, res) => {
+	try {
+		const { studentId } = req.params;
+		const studentBoughtCourses = await StudentCourses.findOne({
+			userId: studentId,
+		});
+
+		res.status(200).json({
+			success: false,
+			data: studentBoughtCourses.courses,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: "Some Error Occured!",
+		});
+	}
+};
+
+module.exports = { getCoursesByStudentId };
