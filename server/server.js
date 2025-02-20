1ab9c424fd2f82c4150ec6c300165d0cba5aba2a
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-20 19:52:34 +0500

    Configure studentProgressRoutes in server.js file

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const instructorCourseRoutes = require("./routes/instructor-routes/course-routes
 const studentCourseRoutes = require("./routes/student-routes/course-routes");
 const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
 const studentCoursesRoutes = require("./routes/student-routes/student-courses-routes");
+const studentCourseProgressRoutes = require("./routes/student-routes/course-progress-routes")
 
 const app = express();
 const PORT = process.env.PORT || 5002;
@@ -37,6 +38,7 @@ app.use("/instructor/course", instructorCourseRoutes);
 app.use("/student/course", studentCourseRoutes);
 app.use("/student/order", studentViewOrderRoutes);
 app.use("/student/courses-bought", studentCoursesRoutes);
+app.use('/student/course-progress/',studentCourseProgressRoutes)
 
 app.use((req, res, next) => {
 	console.log(`${req.method} ${req.path}`);
