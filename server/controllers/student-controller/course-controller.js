3f21e3732f7dff404999376bb352449d07b9a042
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-20 09:22:28 +0500

    Add checkCoursePurchaseInfo that checks whether the student had already purchase the course

--- server/controllers/student-controller/course-controller.js
+++ server/controllers/student-controller/course-controller.js
@@ -1,4 +1,5 @@
 const Course = require("../../models/Course");
+const StudentCourses = require("../../models/StudentCourses");
 
 const getAllStudentViewCourses = async (req, res) => {
 	try {
@@ -79,7 +80,27 @@ const getStudentViewCourseDetails = async (req, res) => {
 	}
 };
 
+const checkCoursePurchaseInfo = async (req, res) => {
+	try {
+		const { id, studentId } = req.params;
+		const studentCourses = await StudentCourses.findOne({ userId: studentId });
+		const ifStudentAlreadyBoughtCurrentCourse =
+			studentCourses.courses.findIndex((item) => item.courseId === id) > -1;
+		res.status(200).json({
+			success: true,
+			data: ifStudentAlreadyBoughtCurrentCourse,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: "Something went wrong,fetching course details error",
+		});
+	}
+};
+
 module.exports = {
 	getAllStudentViewCourses,
 	getStudentViewCourseDetails,
+	checkCoursePurchaseInfo,
 };
