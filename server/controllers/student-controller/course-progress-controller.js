9e2fa255ce04750b4f22e9bbe8e28ad11207c156
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-20 19:40:42 +0500

    Add functionality that gets the current user course progress if the course is purchased

--- server/controllers/student-controller/course-progress-controller.js
+++ server/controllers/student-controller/course-progress-controller.js
@@ -16,6 +16,72 @@ const markCurrentLectureAsViewed = async (req, res) => {
 // get our  current course progress
 const getCurrentCourseProgress = async (req, res) => {
 	try {
+		// get current userId and courseid
+		const { userId, courseId } = req.params;
+
+		//check how many courses the current user have purchased
+		const studentPurchasedCourses = await StudentCourse.findOne({
+			userId,
+		});
+
+		//checks the current course is purchased by the user or not
+		const isCurrentCoursePurchasedByCurrentUser =
+			studentPurchasedCourses?.courses?.findIndex(
+				(item) => item.courseId === courseId
+			) > -1;
+
+		// if the current course is not purchased by the user do this
+		if (!isCurrentCoursePurchasedByCurrentUser) {
+			return res.status(200).json({
+				success: false,
+				data: {
+					isPurchased: false,
+				},
+				message: "You Need To Purchase The Course To Access It!",
+			});
+		}
+
+		// gets the current user course progress from database using courseProgress schema
+		const currentUserCourseProgress = await CourseProgress.findOne({
+			userId,
+			courseId,
+		}).populate("courseId");
+
+		// the current user has that course or not if yes then checks if the progress is started yet or not
+		if (currentUserCourseProgress.lectureProgress.length === 0) {
+			const course = await Course.findById(courseId);
+
+			//if no course is found against the current user id
+			if (!course) {
+				return res.status(404).json({
+					success: false,
+					message: "Course Not Found!",
+				});
+			}
+
+			//the current user not start course yet
+			return res.status(200).json({
+				success: true,
+				message: "No Progress found, you can start watching the course",
+				data: {
+					courseDetails: course,
+					progress: [],
+					isPurchased: true,
+				},
+			});
+		}
+
+		// returns the current user course progress
+		res.status(200).json({
+			success: true,
+			data: {
+				courseDetails: currentUserCourseProgress.courseId,
+				progress: currentUserCourseProgress.lectureProgress,
+				completed: currentUserCourseProgress.completed,
+				completionDate: currentUserCourseProgress.completionDate,
+				isPurchased: true,
+			},
+		});
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({
@@ -35,4 +101,10 @@ const resetCurrentCourseProgress = async (req, res) => {
 			message: "Error Occured in Current course Reset",
 		});
 	}
+};
+
+module.exports = {
+	markCurrentLectureAsViewed,
+	getCurrentCourseProgress,
+	resetCurrentCourseProgress,
 };
