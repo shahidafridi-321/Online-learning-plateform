de82c5423612fcfdd42dcdd8161825d90118d7de
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-21 10:43:26 +0500

    Add routes for reset and mark as viewed for lectures

--- server/routes/student-routes/course-progress-routes.js
+++ server/routes/student-routes/course-progress-routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const {
 	getCurrentCourseProgress,
+	markCurrentLectureAsViewed,
+	resetCurrentCourseProgress,
 } = require("../../controllers/student-controller/course-progress-controller");
 
 const router = express.Router();
 router.get("/get/:userId/:courseId", getCurrentCourseProgress);
-
+router.post("/mark-lecture-viewed", markCurrentLectureAsViewed);
+router.post("/reset-progress", resetCurrentCourseProgress);
 module.exports = router;
