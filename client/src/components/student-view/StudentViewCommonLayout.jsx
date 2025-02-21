e3d79bad833eec72e051394d68b6f15544e4c046
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-21 09:45:22 +0500

    Add logic If the url inclues course-progress it will not display the common header

--- client/src/components/student-view/StudentViewCommonLayout.jsx
+++ client/src/components/student-view/StudentViewCommonLayout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./Header";
 
 export const StudentViewCommonLayout = () => {
+	const location = useLocation();
 	return (
 		<div className="flex flex-col min-h-screen bg-gradient-to-br from-blue-300 via-purple-300 to-pink-300 text-gray-800">
-			<Header />
+			{!location.pathname.includes("course-progress") ? <Header /> : null}
 			<Outlet />
 		</div>
 	);
