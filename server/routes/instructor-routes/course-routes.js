223f61d4ae385068d4c4aa36c6ee6a5728cced5c
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-23 18:03:47 +0500

    Set the getting course end point to users id, And call getMyCourses controllerfunction  instead of getAllCourses

--- server/routes/instructor-routes/course-routes.js
+++ server/routes/instructor-routes/course-routes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const {
-	getAllCourses,
 	addNewCourse,
 	getCourseDetailsById,
 	updateCourseById,
+	getMyCourses,
 } = require("../../controllers/instructor-controller/course-controller");
 
 const router = express.Router();
 
 router.post("/add", addNewCourse);
-router.get("/get", getAllCourses);
+router.get("/get/:id", getMyCourses);
 router.get("/get/detials/:id", getCourseDetailsById);
 router.put("/update/:id", updateCourseById);
 
