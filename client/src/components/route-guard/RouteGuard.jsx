876b295a3138754ddd091e44a967ff89ad1582e0
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-24 12:56:47 +0500

    Refactor The RouteGuard component to allow access to verify-email page

--- client/src/components/route-guard/RouteGuard.jsx
+++ client/src/components/route-guard/RouteGuard.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { Skeleton } from "../ui/skeleton";
 
@@ -9,23 +8,32 @@ export const RouteGuard = ({ authenticated, user, element, loading }) => {
 	if (loading) {
 		return <Skeleton />;
 	}
-	if (!authenticated && !location.pathname.includes("/auth")) {
-		return <Navigate to="/auth" />;
-	}
-	if (
-		authenticated &&
-		user?.role !== "instructor" &&
-		(location.pathname.includes("instructor") ||
-			location.pathname.includes("/auth"))
-	) {
-		return <Navigate to="/home" />;
-	}
-	if (
-		authenticated &&
-		user.role === "instructor" &&
-		!location.pathname.includes("instructor")
-	) {
-		return <Navigate to="/instructor" />;
+
+	// When not authenticated, allow access only to /auth and /verify-email pages.
+	if (!authenticated) {
+		if (
+			location.pathname !== "/auth" &&
+			location.pathname !== "/verify-email"
+		) {
+			return <Navigate to="/auth" />;
+		}
+	} else {
+		// Authenticated users
+		if (user.role !== "instructor") {
+			//  if students trying to access /auth, /verify-email, or any instructor routes, redirect to /home.
+			if (
+				location.pathname === "/auth" ||
+				location.pathname === "/verify-email" ||
+				location.pathname.includes("instructor")
+			) {
+				return <Navigate to="/home" />;
+			}
+		} else {
+			// if instructor not on an instructor route, redirect them to /instructor.
+			if (!location.pathname.includes("instructor")) {
+				return <Navigate to="/instructor" />;
+			}
+		}
 	}
 
 	return <Fragment>{element}</Fragment>;
