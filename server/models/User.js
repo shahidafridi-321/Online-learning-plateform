fe8ab6ecb4b400b49c35ffdbe78fddd21bdc0344
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-24 10:40:53 +0500

    Update User schema , add email verification fields

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,14 @@
+// models/User.js
 const mongoose = require("mongoose");
+
 const userSchema = mongoose.Schema({
 	userName: String,
 	userEmail: String,
 	password: String,
 	role: String,
+	isVerified: { type: Boolean, default: false },
+	verificationCode: String,
+	verificationCodeExpires: Date,
 });
 
 module.exports = mongoose.model("User", userSchema);
