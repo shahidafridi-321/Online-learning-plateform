4b8cc46252b8d159ec84cbb3d059150442cafc84
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-02-24 10:34:03 +0500

    Add endpoint for email-verification

--- server/routes/auth-routes/index.js
+++ server/routes/auth-routes/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const {
 	registerUser,
 	loginUser,
+	verifyEmail,
 } = require("../../controllers/auth-controller/index");
 const authenticateMiddleware = require("../../middleware/auth-middleware");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/verify-email", verifyEmail);
+
 router.get("/check-auth", authenticateMiddleware, (req, res) => {
 	const user = req.user;
 	return res.status(201).json({
