6762d00152f3f49b754232883ff5df97c60b4b0f
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-03-01 10:09:51 +0500

    Add approved field

--- server/models/Reviews.js
+++ server/models/Reviews.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const Reviews = mongoose.Schema({
+const ReviewsSchema = mongoose.Schema({
 	userName: String,
 	image: String,
 	role: String,
 	quote: String,
 	date: Date,
+	approved: Boolean,
 });
 
-module.exports = mongoose.model("review", Reviews);
+module.exports = mongoose.model("review", ReviewsSchema);
--- server/routes/student-routes/review-routes.js
+++ server/routes/student-routes/review-routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
 	getAllReviews,
 	createReview,
+	approveReview,
 } = require("../../controllers/student-controller/review-controller");
 
 const router = express.Router();
 
 router.post("/create/review", createReview);
 router.get("/get/reviews", getAllReviews);
+router.post("/update/review/:id", approveReview);
 
 module.exports = router;
