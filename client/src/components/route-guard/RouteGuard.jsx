8987cffdebeda1fce05b1687793e6c8039ac78e9
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-03-01 15:33:32 +0500

    Logic to Redirect users/admin/instructors to their own pages

--- client/src/components/route-guard/RouteGuard.jsx
+++ client/src/components/route-guard/RouteGuard.jsx
@@ -9,7 +9,6 @@ export const RouteGuard = ({ authenticated, user, element, loading }) => {
 		return <Skeleton />;
 	}
 
-	// When not authenticated, allow access only to /auth and /verify-email pages.
 	if (!authenticated) {
 		if (
 			location.pathname !== "/auth" &&
@@ -18,20 +17,35 @@ export const RouteGuard = ({ authenticated, user, element, loading }) => {
 			return <Navigate to="/auth" />;
 		}
 	} else {
-		// Authenticated users
-		if (user.role !== "instructor") {
-			//  if students trying to access /auth, /verify-email, or any instructor routes, redirect to /home.
+		if (user.role === "admin") {
+			if (!location.pathname.startsWith("/admin")) {
+				return <Navigate to="/admin/approve-reject-review" />;
+			}
+		} else if (user.role === "instructor") {
+			if (!location.pathname.startsWith("/instructor")) {
+				return <Navigate to="/instructor" />;
+			}
+		} else {
+			// For students
 			if (
-				location.pathname === "/auth" ||
-				location.pathname === "/verify-email" ||
-				location.pathname.includes("instructor")
+				location.pathname.startsWith("/admin") ||
+				location.pathname.startsWith("/instructor")
 			) {
 				return <Navigate to="/home" />;
 			}
-		} else {
-			// if instructor not on an instructor route, redirect them to /instructor.
-			if (!location.pathname.includes("instructor")) {
+		}
+
+		// Redirect authenticated users away from /auth and /verify-email
+		if (
+			location.pathname === "/auth" ||
+			location.pathname === "/verify-email"
+		) {
+			if (user.role === "admin") {
+				return <Navigate to="/admin/approve-reject-review" />;
+			} else if (user.role === "instructor") {
 				return <Navigate to="/instructor" />;
+			} else {
+				return <Navigate to="/home" />;
 			}
 		}
 	}
