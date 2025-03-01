07cd57742fd8eb9030c88ac39f36cc03035b1ab5
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-03-01 15:25:31 +0500

    Add end points to delete review and get latest reviews

--- server/routes/student-routes/review-routes.js
+++ server/routes/student-routes/review-routes.js
@@ -3,12 +3,16 @@ const {
 	getAllReviews,
 	createReview,
 	approveReview,
+	deleteReview,
+	getLatestReviews,
 } = require("../../controllers/student-controller/review-controller");
 
 const router = express.Router();
 
 router.post("/create/review", createReview);
 router.get("/get/reviews", getAllReviews);
+router.get("/get/latest-reviews", getLatestReviews);
 router.post("/update/review/:id", approveReview);
+router.delete("/delete/review/:id", deleteReview);
 
 module.exports = router;
