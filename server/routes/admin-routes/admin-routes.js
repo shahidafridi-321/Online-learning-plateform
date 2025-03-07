4bdd7edc426c4d90a02d738b54e1e346062ebb1a
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-03-07 15:29:58 +0500

    Add endpoint to handle update user requests

--- server/routes/admin-routes/admin-routes.js
+++ server/routes/admin-routes/admin-routes.js
@@ -3,6 +3,7 @@ const {
 	getAllCourses,
 	getAllReviews,
 	getAllUsers,
+	updateUser
 } = require("../../controllers/admin-controller/admin-controller");
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/courses/all", getAllCourses);
 router.get("/reviews/all", getAllReviews);
 router.get("/users/all", getAllUsers);
+router.put("/users/:userId", updateUser);
 
 module.exports = router;
