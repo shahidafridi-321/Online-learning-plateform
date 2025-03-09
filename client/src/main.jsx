55d6cf390482293a163ba108cbea5aa96b3af2ba
Shahid Afridi <shahidrahmansr87@gmail.com>
2025-03-09 11:56:09 +0500

    Render Toaster component from react-sooner

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,13 @@ import { AuthProvider } from "./context/auth-context";
 import App from "./App";
 import { InstructorContextProvider } from "./context/instructor-context/InstructorContext";
 import { StudentContextProvider } from "./context/student-context/StudentContext";
+import { Toaster } from "react-sonner";
 
 createRoot(document.getElementById("root")).render(
 	<AuthProvider>
 		<InstructorContextProvider>
 			<StudentContextProvider>
+				<Toaster position="top-right" richColors />
 				<App />
 			</StudentContextProvider>
 		</InstructorContextProvider>
